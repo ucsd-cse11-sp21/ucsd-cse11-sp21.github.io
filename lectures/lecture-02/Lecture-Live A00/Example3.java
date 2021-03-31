4cad13cdc033ea65025da4b71eca5cda17f410f8
Greg <59540827+ucsd-cse12-w20@users.noreply.github.com>
2021-03-31 09:56:28 -0700

    Lecture 2 updates

---
+++ lectures/lecture-02/Lecture-Live A00/Example3.java
@@ -0,0 +1,20 @@
+class Example3 {
+    int numberOfStaff = 14;
+    //String name = "Greg Miranda";   //String value, string literal
+
+    String firstName = "Greg";
+    String lastName = "Miranda";
+    String space = " ";
+
+    // + concncates or appends strings
+    String fullName = this.firstName + this.space + this.lastName;
+
+    String str = this.firstName + 2;
+    String str1 = this.firstName + "2";
+
+    String className = "11" + 200;
+    String klassName = 11 + "200";
+
+    String value = 1 + 2 + 3 + " 11 " + 4 + 5 + 6;
+    String value1 = 1 + 2 + 3 + " 11 " + (4 + 5 + 6);
+}
---
+++ lectures/lecture-02/Lecture-Live B00/Examples3.java
@@ -0,0 +1,19 @@
+class Examples3 {
+    int numberOfStaff = 14;
+    //String name = "Greg Miranda";
+    String firstName = "Greg";
+    String lastName = "Miranda";
+    String space = " ";
+
+    // + concanates or appends string
+    String fullName = this.firstName + this.space + this.lastName;
+
+    String str = this.firstName + 2;
+    String str1 = this.firstName + "2";
+
+    String className = "11" + 200;
+    String klassName = 11 + "200";
+
+    String value = 1 + 2 + 3 + " 11 " + 4 + 5 + 6;
+    String value1 = 1 + 2 + 3 + " 11 " + (4 + 5 + 6);
+}
