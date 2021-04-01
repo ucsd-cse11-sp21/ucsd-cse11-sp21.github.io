e3e61eb42d0e76a1e1d594da5aa5e5aa3cde73a2
Ayon <ayonbiswas97@gmail.com>
2021-03-31 21:55:55 -0700

    adding discussion1 code and slides

---
+++ discussions/discussion-01/A01/DiscussionExample.java
@@ -0,0 +1,22 @@
+class DiscussionExample{
+    int x = 1 + 1;
+    int exampleMethod(int y, int z){
+        return this.x + y + z;
+    }
+
+    int ans = exampleMethod(1, 2);
+
+    String value1 = 1 + 2 + 3 + " 11 " + (4 + 5 + 6);
+    
+    //method takes in int and return int
+    // int perimeter(int width, int height){
+
+	// return 
+	// }
+
+	// int borderArea(int w1, int l1, int w2, int l2){
+
+	// return 
+	// }
+    
+}
