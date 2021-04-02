7f164b3d5307f56119702e23866c4eb47bf4bf70
Greg <59540827+ucsd-cse12-w20@users.noreply.github.com>
2021-04-02 09:59:54 -0700

    Lecture 3 update

---
+++ lectures/lecture-03/Lecture-Live A00/MethodExample.java
@@ -0,0 +1,11 @@
+class MethodExample {
+    //int sos1 = 3 * 3 + 5 * 5;
+    //int sos2 = 4 * 4 + 7 * 7;
+
+    int sumSquares(int n, int m) {
+        return n * n + m * m;
+    }
+
+    int ans1 = this.sumSquares(3, 5);
+    int ans2 = this.sumSquares(4, 7);
+}
