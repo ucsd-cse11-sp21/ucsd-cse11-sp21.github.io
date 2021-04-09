40646b94aae81cd8a8e206938080a5935852d9f9
Greg <59540827+ucsd-cse12-w20@users.noreply.github.com>
2021-04-09 09:54:19 -0700

    Lecture 6 update

--- lectures/lecture-06/Lecture-Live A00/ExamplesLec.java
+++ lectures/lecture-06/Lecture-Live A00/ExamplesLec.java
@@ -1,3 +1,36 @@
+class Point {
+    int x;  //uninitialzied field definitions
+    int y;
+
+    // constructor
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    //quadrant - takes no parameters, returns a String
+    //of which qudrant the Point is in
+    String quadrant() {
+        if (this.x > 0 && this.y > 0) { return "1"; }
+        else if (this.x < 0 && this.y > 0) { return "2"; }
+        else if (this.x < 0 && this.y < 0) { return "3"; }
+        else if (this.x > 0 && this.y < 0) { return "4"; }
+        else {
+            return "On axis";
+        }
+    }
+}
+
 class ExamplesLec {
+
+    int examplesOfNum = 4 + 5;
+
+    Point fourFivePoint = new Point(4, 5);
+    Point negOneThreePoint = new Point(-1, -3);
+    Point fourFivePointAgain = this.fourFivePoint;
+
+    //String quadA = this.Point.quadrant(); //does not work - no Point field in this class
+    String quadA = this.fourFivePoint.quadrant();       //"1"
+    String quadB = this.negOneThreePoint.quadrant();    //"3"
 
 }
--- lectures/lecture-06/Lecture-Live B00/ExamplesLec.java
+++ lectures/lecture-06/Lecture-Live B00/ExamplesLec.java
@@ -1,3 +1,34 @@
-class ExamplesLec {
+class Point {
+    int x;  //unitialzied field definitions
+    int y;
 
+    // constructor
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    //quadrant - takes no parameters, and returns a String
+    //of which qudrant the point is in
+    String quadrant() {
+        if (this.x > 0 && this.y > 0) { return "1"; }
+        else if (this.x < 0 && this.y > 0) { return "2"; }
+        else if (this.x < 0 && this.y < 0) { return "3"; }
+        else if (this.x > 0 && this.y < 0) { return "4"; }
+        else {
+            return "On axis";
+        }
+    }
+}
+
+class ExamplesLec {
+    int examplesOfNum = 4 + 5;
+
+    Point fourFivePoint = new Point(4, 5);
+    Point negOneThreePoint = new Point(-1, -3);
+    Point fourFivePointAgain = this.fourFivePoint;
+
+    //String quadA = Point.quadrant();    //doesn't work, there is no object
+    String quadA = this.fourFivePoint.quadrant();       //"1"
+    String quadB = this.negOneThreePoint.quadrant();    //"3"
 }
