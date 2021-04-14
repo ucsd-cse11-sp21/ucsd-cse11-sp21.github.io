53355b4f6c6c659415af59fb640ae9474d2569d4
Greg <59540827+ucsd-cse12-w20@users.noreply.github.com>
2021-04-14 09:53:24 -0700

    Lecture 8 update

--- lectures/lecture-08/Lecture-Live A00/ExamplesLec.java
+++ lectures/lecture-08/Lecture-Live A00/ExamplesLec.java
@@ -1,3 +1,10 @@
 class ExamplesLec {
 
+    double sqrt2 = Math.sqrt(2);
+    double sqrt2FromDouble = Math.sqrt(2.0);
+
+    double cubeOf12 = Math.pow(12, 3);
+
+    double maxOf45  = Math.max(4, 5);
+
 }
