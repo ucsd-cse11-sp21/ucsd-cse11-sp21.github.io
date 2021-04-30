34cc8a430756e31f582944aade1f3992166c4c4e
Greg <59540827+ucsd-cse12-w20@users.noreply.github.com>
2021-04-29 21:52:05 -0700

    Lecture 15

---
+++ lectures/lecture-15/Lecture-Live A00/VariablesExamples.java
@@ -0,0 +1,18 @@
+public class VariablesExamples {
+    
+    public static void main(String[] args) {
+        
+        int x = 10;
+        //int x = 15;   disallowed, erorr
+        System.out.println(x);
+        x = 15;
+        System.out.println(x);
+
+
+        
+        //int y = x + 5;
+        //String s = "abc" + "def";
+        //System.out.println(s);
+    }
+
+}
