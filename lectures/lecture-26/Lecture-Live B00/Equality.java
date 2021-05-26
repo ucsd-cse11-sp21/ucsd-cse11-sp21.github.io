ec9a92b679955e0f824987a26a726c5013909d5b
Greg <59540827+ucsd-cse12-w20@users.noreply.github.com>
2021-05-26 09:54:03 -0700

    Lecture 26 updates

--- lectures/lecture-26/Lecture-Live B00/Equality.java
+++ lectures/lecture-26/Lecture-Live B00/Equality.java
@@ -1,8 +1,42 @@
+import java.util.Arrays;
+
 class Point {
     int x, y;
     Point(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    /**
+     * 
+     * @param other Another point to compare to
+     * @return true if the points have the same x and y coordinates, false otherwise
+     */
+    boolean same(Point other) {
+        return this.x == other.x && this.y == other.y;
+    }
+
+    // This is an OVERLOADED equals method, the one in Object class
+    // has a different signature: boolean equals(Object other)
+    boolean equals(Point other) {
+        //System.out.println("Calling our equals method now!");
+        return this.x == other.x && this.y == other.y;
+    }
+
+    // Implementation of the equals method that is defined in Object class.
+    // This method OVERRIDEs the equals method in the Object class.
+    public boolean equals(Object other) {
+        System.out.println("Calling our equals method now!");
+        if (other == null) {
+            return false;
+        }
+        if (!(other instanceof Point)) {
+            return false;
+        }
+
+        // other must be a Point here
+        Point p = (Point) other;
+        return this.x == p.x && this.y == p.y;
     }
 }
 
@@ -14,10 +48,37 @@ class Equality {
 
     boolean pAndP2EqualsByOperator = p == p2;
     boolean pAndP2Equals = p.equals(p2);    //Calling the .equals method on built-in Object class
+    boolean pAndP2SameByMethod = p.same(p2);
+
+    Point[] points1 = { p, p2 };
+    Point[] points2 = { p2, p };
+    boolean pointsEqualByArraysEqual = Arrays.equals(points1, points2);
 
 
 
 
+
+
+
+
+    int[] numsA = { 5, 1, 7 };
+    int[] numsB = { 5, 1, 7 };
+    boolean numsEqualsByOperator = (numsA == numsB);
+    boolean numsEqualByMethod = numsA.equals(numsB);
+    boolean numsEqualByArraysEqual = Arrays.equals(numsA, numsB);
+    // calls .equals() on all elements if they are objects
+    // or compares with == if they are primitives
+
+    String s = "apple";
+    String s2 = "applesauce".substring(0, 5);   //ensures we get a new string reference for s2
+    String[] strs1 = { s, s2 };
+    String[] strs2 = { s2, s };
+    boolean strsEqualsByOperator = (strs1 == strs2);
+    boolean strsEqualByMethod = strs1.equals(strs2);
+    boolean strsEqualByArraysEqual = Arrays.equals(strs1, strs2);
+
+    boolean sAndS2EqualsByOperator = (s == s2);
+    boolean sAndS2EqualsByMethod = s.equals(s2);
 
     public static void main(String[] args) {
         String appleVar1 = "apple";
--- lectures/lecture-26/Lecture-Live B00/JavaFun.java
+++ lectures/lecture-26/Lecture-Live B00/JavaFun.java
@@ -1,4 +1,30 @@
+class A { }
+class B extends A { }
+class C extends A { }
+
+
 class JavaFun {
+
+    C c = new C();
+    B b = new B();
+    A a = new A();
+    A aTypedVariableHoldingAC = new C();
+
+    boolean aInstanceOfA = a instanceof A;
+    boolean bInstanceOfB = b instanceof B;
+    boolean cInstanceOfC = c instanceof C;
+
+    boolean cInstanceOfA = c instanceof A;
+    // Java won't let us run the next line
+    //boolean cInstanceOfB = c instanceof B;
+    boolean aInstanceOfC = a instanceof C;
+    boolean cInAVarInstanceOfC = aTypedVariableHoldingAC instanceof C;
+    boolean cInstanceOfObject = c instanceof Object;
+
+
+
+
+
     int x = 2147483647;
     int y = -2147483648;
 
